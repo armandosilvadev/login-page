e99f691dd6f1f6c8b17db8ede2e78b4ac8ae4755
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-07-18 12:39:54 -0300

    add controller to get the currently user

--- login-page-backend/src/main/java/dev/armando/login_page_backend/user/UserController.java
+++ login-page-backend/src/main/java/dev/armando/login_page_backend/user/UserController.java
@@ -1,6 +1,8 @@
 package dev.armando.login_page_backend.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> me(Authentication authentication) {
+        User user =  (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(new UserResponse(user.getUsername()));
+    }
 }
---
+++ login-page-backend/src/main/java/dev/armando/login_page_backend/user/UserResponse.java
@@ -0,0 +1,4 @@
+package dev.armando.login_page_backend.user;
+
+public record UserResponse(String username) {
+}
