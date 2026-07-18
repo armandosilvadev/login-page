c83aab8a3c188fd9aa8236ff82cec03195b0b099
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-07-18 07:22:34 -0300

    add private final and @RequiredArgsConstructor

--- login-page-backend/src/main/java/dev/armando/login_page_backend/auth/AuthenticationController.java
+++ login-page-backend/src/main/java/dev/armando/login_page_backend/auth/AuthenticationController.java
@@ -4,6 +4,7 @@ import dev.armando.login_page_backend.infra.security.TokenService;
 import dev.armando.login_page_backend.user.User;
 import dev.armando.login_page_backend.user.UserRepository;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,13 +15,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
-    @Autowired
-    TokenService tokenService;
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UserRepository userRepository;
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationResponse data) {
