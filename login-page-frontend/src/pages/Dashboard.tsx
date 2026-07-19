ee3c199e6ff5f42f5d9bc8c9ffc0fea658f2e8f0
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-07-19 09:48:32 -0300

    update dashboard

--- login-page-frontend/src/pages/Dashboard.tsx
+++ login-page-frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import useUser from '../auth/hooks/useUser';
 
 const Dashboard = () => {
   const { data, isLoading } = useUser();
 
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+
   return (
     <>
       <h1>{isLoading ? 'Loading...' : `Hello ${data?.username}`}</h1>
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </>
   );
 };
